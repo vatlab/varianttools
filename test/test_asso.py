130b67ac53470583b798dc0d7fb229f5235a3eb8
bpeng2000 <bpeng2000@bfd2a824-b778-4082-b236-bff916e0c2bb>
2011-09-02 03:54:53 +0000

    Do not let test_asso stop run_tests.py

--- test/test_asso.py
+++ test/test_asso.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 
-import variant_tools.assoTests as t
+import sys
+try:
+    import variant_tools.assoTests as t
+except:
+    pass
 
-data = t.AssoData()
-data.setPhenotype([1, 2, 1, 2])
-data.setGenotype([
-    [1, 2, 3, 4],
-    [2, 1, 2, 1],
-    [1, 2, 2, 2],
-    [1, 2, 3, 2]
-    ])
+if __name__ == '__main__':
+    data = t.AssoData()
+    data.setPhenotype([1, 2, 1, 2])
+    data.setGenotype([
+        [1, 2, 3, 4],
+        [2, 1, 2, 1],
+        [1, 2, 2, 2],
+        [1, 2, 3, 2]
+        ])
 
-a = t.SumToX()
-a.apply(data)
-print data.phenotype()
-print data.genotype()
+    a = t.SumToX()
+    a.apply(data)
+    print data.phenotype()
+    print data.genotype()
 
 #
-p = t.PhenoPermutator(100, [t.SomeTest()])
-print p.permute(data)
+    p = t.PhenoPermutator(100, [t.SomeTest()])
+    print p.permute(data)
