65e50403584ddf0c3c5821cbc6302a3349b7d0c0
bpeng2000 <bpeng2000@bfd2a824-b778-4082-b236-bff916e0c2bb>
2011-10-05 14:32:19 +0000

    Allow vtools show table sample_variant_? (without $name_genotype)

--- variant_tools/project.py
+++ variant_tools/project.py
@@ -1608,7 +1608,10 @@ def show(args):
                 if table in [x.name for x in proj.annoDB]:
                     table = '{0}.{0}'.format(table)
                 if not proj.db.hasTable(table):
-                    raise ValueError('Table {} does not exist'.format(table))
+                    if table.startswith('sample_variant_') and proj.db.hasTable('{}_genotype.{}'.format(proj.name, table)):
+                        table = '{}_genotype.{}'.format(proj.name, table)
+                    else:
+                        raise ValueError('Table {} does not exist'.format(table))
                 # print content of table
                 headers = proj.db.getHeaders(table)
                 print(', '.join(headers))
