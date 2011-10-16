544231b27e200a3136b59529982e975cbbd7ff87
bpeng2000 <bpeng2000@bfd2a824-b778-4082-b236-bff916e0c2bb>
2011-10-16 23:07:06 +0000

    Release rc2

--- variant_tools/project.py
+++ variant_tools/project.py
@@ -39,7 +39,7 @@ from subprocess import Popen, PIPE
 from collections import namedtuple, defaultdict
 from .utils import DatabaseEngine, ProgressBar, setOptions, SQL_KEYWORDS, delayedAction, filesInURL, downloadFile
 
-VTOOLS_VERSION = '1.0rc1'
+VTOOLS_VERSION = '1.0rc2'
 VTOOLS_COPYRIGHT = '''variant tools version {} : Copyright (c) 2011 Bo Peng.'''.format(VTOOLS_VERSION)
 VTOOLS_CITE = '''Please cite Anthony et al ....''' # pending
 VTOOLS_CONTACT = '''Please visit http://varianttools.sourceforge.net for more information.'''
