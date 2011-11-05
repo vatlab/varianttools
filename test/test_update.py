0cfc14a7061b42a442cb9e6dd1c2da02b34007e4
bpeng2000 <bpeng2000@bfd2a824-b778-4082-b236-bff916e0c2bb>
2011-11-05 15:02:55 +0000

    Shuffling files around for the merge

--- test/test_update.py
+++ test/test_update.py
@@ -28,7 +28,7 @@ import os
 import glob
 import unittest
 import subprocess
-from testUtils import ProcessTestCase, runCmd, numOfSample, numOfVariant, outputOfCmd
+from testUtils import ProcessTestCase, runCmd, numOfSample, numOfVariant, outputOfCmd, initTest, output2list
 
 class TestImportVCF(ProcessTestCase):
     def setUp(self):
@@ -57,6 +57,45 @@ class TestImportVCF(ProcessTestCase):
         self.assertSucc('vtools update variant --format fmt/dbSNP_hg19validation txt/dbSNP_hg19validation.txt --build hg19')
         self.assertEqual(outputOfCmd('vtools select variant "mut_type_dbSNP is not null" -c'), '175\n')
         self.assertOutput("vtools select variant alt_pos=753405 -o chr pos mut_type_dbSNP validation", "1\t743268\tuntranslated-5\tby-cluster,by-1000genomes\n")
+        
+    def testSampleStat(self):
+        'Test command vtools update'
+        runCmd('vtools import vcf/CEU.vcf.gz --build hg18')
+        runCmd('vtools phenotype --from_file phenotype/phenotype.txt')
+        runCmd('vtools select variant --samples "filename like \'%CEU%\'" -t CEU')
+        self.assertFail('vtools update')
+        self.assertSucc('vtools update -h')
+        self.assertFail('vtools update "num=#(alt)"')
+        self.assertSucc('vtools update variant --from_stat "num=#(alt)" "hom=#(hom)" "het=#(het)" "other=#(other)"')
+        total = int(outputOfCmd("vtools execute 'select sum(num) from variant'").split('\n')[0])
+        hom = int(outputOfCmd("vtools execute 'select sum(hom) from variant'").split('\n')[0])
+        het = int(outputOfCmd("vtools execute 'select sum(het) from variant'").split('\n')[0])
+        other = int(outputOfCmd("vtools execute 'select sum(other) from variant'").split('\n')[0])
+        self.assertEqual(total, hom*2+het+other)
+        self.assertSucc('vtools update CEU --from_stat "CEU_num=#(alt)# -s "filename like \'%CEU%\'"')
+        self.assertEqual(int(outputOfCmd("vtools execute 'select sum(CEU_num) from CEU'").split('\n')[0]), 6383)
+        self.assertSucc('vtools update CEU --from_stat "CEU_num=#(alt)" "CEU_hom=#(hom)" "CEU_het=#(het)" "CEU_other=#(other)"  --samples "filename like \'%CEU%\'"')
+        self.assertSucc('vtools update CEU --from_stat "CEU_cases_het=#(het)"--samples "filename like \'%CEU%\' and aff=\'2\'"')
+        self.assertEqual(int(outputOfCmd("vtools execute 'select sum(CEU_cases_het) from CEU'").split('\n')[0]), 1601)
+        self.assertSucc('vtools update CEU --from_stat "CEU_strls_het=#(het)" -s "filename like \'%CEU%\' and aff=\'1\'"')
+        
+    def testGenotypeSumStats(self):
+        'Test command vtools update min/max/sum/mean_FIELD'
+        runCmd('vtools import --format fmt/missing_gen vcf/missing_gen.vcf --build hg19')
+        # non-existing field, should fail
+        self.assertFail('vtools update variant --from_stat "max_gq=max(GQ1)" "min_gq=min(GQ)"')
+        self.assertSucc('vtools update variant --from_stat "max_gq=max(GQ)" "min_gq=min(GQ)" "mean_gq=avg(GQ)"')
+        out = output2list('vtools output variant max_gq min_gq mean_gq')
+        self.assertEqual(out[0], 'NA\tNA\tNA')
+        self.assertTrue(out[3].startswith('100\t15\t69.3333'))
+        self.assertTrue(out[4].startswith('6\t3\t4.0'))
+        self.assertTrue(out[5].startswith('4\t3\t3.33333'))
+        self.assertSucc('vtools update variant --from_stat "total_dp=sum(GD)"')
+        self.assertEqual(output2list('vtools output variant total_dp'), ['NA', 'NA', 'NA', '60', '7', '4'])
+        # ffilter out variants having GQ less than 4,
+        # then for each remining variant count the total number of alt genotypes across all samples
+        self.assertSucc('vtools update variant --from_stat "gq_ge_4=#(alt)" --genotype "GQ >= 4"')
+        self.assertEqual(output2list('vtools output variant gq_ge_4'), ['0', '0', '0', '0', '3', '1'])
         
 if __name__ == '__main__':
     unittest.main()
