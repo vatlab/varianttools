124afe75c806457e7e3da74e276b0b504cc29e11
qiaomin <qiaomin@bfd2a824-b778-4082-b236-bff916e0c2bb>
2012-02-28 21:26:28 +0000

    updated a test where error message from C++ module is not yet properly handled

--- test/test_associate.py
+++ test/test_associate.py
@@ -8,44 +8,44 @@ from testUtils import ProcessTestCase, runCmd, outputOfCmd
 from zipfile import ZipFile
 
 class TestAsso(ProcessTestCase):
-  def setUp(self):
-      'Create a project'
-      #runCmd('vtools init test -f')
-      #runCmd('vtools import --format txt/assoc.fmt txt/assoc.dat --build hg18')
-      #runCmd('vtools phenotype --from_file txt/assoc.phen)
-      #too slow to build the test database. Better unzip a pre-stored project
-      zip = ZipFile('txt/assoproj.zip')
-      dir = os.getcwd()
-      zip.extractall(dir)
 
   def removeProj(self):
       runCmd('vtools remove project')
 
-  def testBasicAsso(self):
-      'Test basic regression routines'
+  def testAssoSet1(self):
+      'Test set1 for association module'
+      zip = ZipFile('txt/assoproj.zip')
+      dir = os.getcwd()
+      zip.extractall(dir)
       # basic commands
       self.assertSucc('vtools associate -h')
-      self.assertSucc('vtools associate variant filename --method LinearBurdenTest -h')
-      self.assertSucc('vtools associate variant phen2 -m "LinearBurdenTest"')
-      self.assertSucc('vtools associate variant phen2 -m "LinearBurdenTest" -g chr')
+      self.assertSucc('vtools associate variant filename --method LNBT -h')
+      self.assertSucc('vtools associate variant phen2 -m "LNBT"')
+      self.assertSucc('vtools associate variant phen2 -m "LNBT" -g chr')
       # regression methods
-      self.assertSucc('vtools associate variant phen2 --covariate phen1 -m "LinearBurdenTest" -g chr')
-      self.assertSucc('vtools associate variant phen2 --covariate phen1 -m "LinearBurdenTest"')
-      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LinearBurdenTest" -g chr')
-      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LinearBurdenTest -q1 0.05 -q2 0.001" -g chr')
-      self.assertFail('vtools associate variant phen2 --covariate phen1 phen3 -m "LinearBurdenTest -q1 5 -q2 0.001" -g chr')
-      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LinearBurdenTest --use_indicator" -g chr')
-      self.assertSucc('vtools associate variant phen2 -m "LinearBurdenTest --alternative 2" -g chr')
-      self.assertFail('vtools associate variant phen2 -m "LinearBurdenTest --alternative 8" -g chr')
+      self.assertSucc('vtools associate variant phen2 --covariate phen1 -m "LNBT" -g chr')
+      self.assertSucc('vtools associate variant phen2 --covariate phen1 -m "LNBT"')
+      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LNBT" -g chr')
+      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LNBT -q1 0.05 -q2 0.001" -g chr')
+      self.assertFail('vtools associate variant phen2 --covariate phen1 phen3 -m "LNBT -q1 5 -q2 0.001" -g chr')
+      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LNBT --use_indicator" -g chr')
+      self.assertSucc('vtools associate variant phen2 -m "LNBT --alternative 2" -g chr')
+      self.assertFail('vtools associate variant phen2 -m "LNBT --alternative 8" -g chr')
       # permutation based tests
-      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LinearBurdenTest -p 1000" -g chr')
-      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LinearBurdenTest -p 1000 --permute_by x" -g chr')
-      self.assertFail('vtools associate variant phen2 --covariate phen1 phen3 -m "LinearBurdenTest -p 1000 --permute_by M" -g chr')
-      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LinearBurdenTest -p 10000000 --adaptive 0.000001"')
-      self.assertFail('vtools associate variant phen2 --covariate phen1 phen3 -m "LinearBurdenTest -p 10000000 --adaptive 24"')
-      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LinearBurdenTest -p 1000 --variable_thresholds"')
+      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LNBT -p 1000" -g chr')
+      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LNBT -p 1000 --permute_by x" -g chr')
+      self.assertFail('vtools associate variant phen2 --covariate phen1 phen3 -m "LNBT -p 1000 --permute_by M" -g chr')
+      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LNBT -p 10000000 --adaptive 0.000001"')
+      self.assertFail('vtools associate variant phen2 --covariate phen1 phen3 -m "LNBT -p 10000000 --adaptive 24"')
+      self.assertSucc('vtools associate variant phen2 --covariate phen1 phen3 -m "LNBT -p 1000 --variable_thresholds"')
       
-      
+  def testAssoSet2(self):
+      'Test set2 for association module'
+      zip = ZipFile('txt/assoproj2.zip')
+      dir = os.getcwd()
+      zip.extractall(dir)
+      self.assertSucc('vtools associate variant BMI --samples "RACE=0" --covariate SEX -m "LNBT" -j8')
+
       
       
 if __name__ == '__main__':
