b1bd42a954c4ae2a13a6ecbde1e174200cf1bc40
lma5 <lma5@bfd2a824-b778-4082-b236-bff916e0c2bb>
2012-04-04 20:43:27 +0000

    Added a test to check the update information from annotation database

--- test/test_execute.py
+++ test/test_execute.py
@@ -28,7 +28,7 @@ import os
 import glob
 import unittest
 import subprocess
-from testUtils import ProcessTestCase, runCmd, initTest
+from testUtils import ProcessTestCase, runCmd, initTest, output2list
 
 class TestExecute(ProcessTestCase):
     def setUp(self):
@@ -47,6 +47,30 @@ class TestExecute(ProcessTestCase):
         self.assertSucc('vtools execute \'delete FROM variant where chr=1\' -v2')
         self.assertSucc('vtools execute \'delete FROM variant where ref="T"\' -v2')
         self.assertSucc('vtools execute \'delete FROM sample where aff=2\' -v2')
+    
+    def testExeAnno(self):
+        runCmd('vtools init test -f')
+        runCmd('vtools import vcf/SAMP1.vcf --build hg19')
+        runCmd('vtools import vcf/SAMP2.vcf')
+        runCmd('vtools use refGene')
+        runCmd('vtools update variant --set ref_name=refGene.name')
+        self.assertSucc('vtools execute \'select chr,txStart,txEnd,name from refGene where name is not null\'')
+        self.assertOutput('vtools execute \'select chr,txStart,txEnd,name from refGene where name="NR_024321"\'','1\t761586\t762902\tNR_024321\n')
+        out2 = output2list('vtools execute \'select chr from variant where ref_name = "NR_024321"\'')
+        for y in out2:
+            print(y)
+            if int(y) != 1:
+               raise ValueError('The chromosome numbers are not same in variant table and annotation file')            
+        pass
+
+        out1 = output2list('vtools execute \'select pos from variant where ref_name = "NR_024321"\'')
+        for x in out1:
+            print(x)
+            if int(x) < 761586:
+               raise ValueError('Out of the range of the gene position')            
+            if int(x) > 762902:
+               raise ValueError('Out of the range of the gene position')
+        pass
 
 if __name__ == '__main__':
     unittest.main()
