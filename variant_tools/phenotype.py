470b982151caa5ad8b1b6be4c53d8ca5d40d0c7d
bpeng2000 <bpeng2000@bfd2a824-b778-4082-b236-bff916e0c2bb>
2012-05-14 02:16:33 +0000

    Revert revision 1133 because it is better to use command "vtools admin rename_sample" to explicitly rename samples than using "vtools phenotype"

--- variant_tools/phenotype.py
+++ variant_tools/phenotype.py
@@ -204,6 +204,8 @@ class Sample:
         cur_fields = self.db.getHeaders('sample')[3:]
         # handle each field one by one
         for idx, field in enumerate(new_fields):
+            if field.lower() == 'sample_name':
+                raise ValueError('Command vtools phenotype cannot be used to update sample names. Please use command "vtools admin" for this purpose.')
             if allowed_fields and field.lower() not in [x.lower() for x in allowed_fields]:
                 self.logger.debug('Ignoring field {}'.format(field))
                 continue
@@ -266,7 +268,7 @@ class Sample:
         if expression != 'NULL' and fldType is None:
             raise ValueError('Cannot determine the type of the expression')
         # if adding a new field
-        cur_fields = self.db.getHeaders('sample')[3:] + ['sample_name']
+        cur_fields = self.db.getHeaders('sample')[3:]
         if field.lower() not in [x.lower() for x in cur_fields]:
             if field.upper in SQL_KEYWORDS:
                 raise ValueError("Phenotype name '{}' is not allowed because it is a reserved word.".format(x))
@@ -410,8 +412,7 @@ def phenotypeArguments(parser):
             of the number of high quality variants to the number of all variants, where
             high_qt and all_qt are obtained from sample statistics using parameter
             --from_stat). Parameter --samples could be used to limit the samples for
-            which genotypes will be set. This option can also be used to update
-            sample_name (e.g. --set sample_name="s1" --samples 'filename like "s1%%"').'''),
+            which genotypes will be set.'''),
     parser.add_argument('--from_stat', nargs='*', metavar='EXPRESSION', default=[],
         help='''Set a phenotype to a summary statistics of a genotype field. For 
             example, "num=count(*)" sets phenotype num to be the number of genotypes
