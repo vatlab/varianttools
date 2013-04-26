9bd7e942fe761154c46a247fb51003680185f214
bpeng2000 <bpeng2000@bfd2a824-b778-4082-b236-bff916e0c2bb>
2013-04-26 14:15:42 +0000

    Create a logger object right after the creation of env so that a logger will be usable even if a project is failed to create

--- source/utils.py
+++ source/utils.py
@@ -312,6 +312,72 @@ class RuntimeEnvironments(object):
     search_path = property(lambda self: self._search_path, _set_search_path)
     #
     # attribute logger
+    class ColoredFormatter(logging.Formatter):
+        # A variant of code found at http://stackoverflow.com/questions/384076/how-can-i-make-the-python-logging-output-to-be-colored
+        def __init__(self, msg):
+            logging.Formatter.__init__(self, msg)
+            self.LEVEL_COLOR = {
+                'DEBUG': 'BLUE',
+                'WARNING': 'PURPLE',
+                'ERROR': 'RED',
+                'CRITICAL': 'RED_BG',
+                }
+            self.COLOR_CODE={
+                'ENDC':0,  # RESET COLOR
+                'BOLD':1,
+                'UNDERLINE':4,
+                'BLINK':5,
+                'INVERT':7,
+                'CONCEALD':8,
+                'STRIKE':9,
+                'GREY30':90,
+                'GREY40':2,
+                'GREY65':37,
+                'GREY70':97,
+                'GREY20_BG':40,
+                'GREY33_BG':100,
+                'GREY80_BG':47,
+                'GREY93_BG':107,
+                'DARK_RED':31,
+                'RED':91,
+                'RED_BG':41,
+                'LIGHT_RED_BG':101,
+                'DARK_YELLOW':33,
+                'YELLOW':93,
+                'YELLOW_BG':43,
+                'LIGHT_YELLOW_BG':103,
+                'DARK_BLUE':34,
+                'BLUE':94,
+                'BLUE_BG':44,
+                'LIGHT_BLUE_BG':104,
+                'DARK_MAGENTA':35,
+                'PURPLE':95,
+                'MAGENTA_BG':45,
+                'LIGHT_PURPLE_BG':105,
+                'DARK_CYAN':36,
+                'AUQA':96,
+                'CYAN_BG':46,
+                'LIGHT_AUQA_BG':106,
+                'DARK_GREEN':32,
+                'GREEN':92,
+                'GREEN_BG':42,
+                'LIGHT_GREEN_BG':102,
+                'BLACK':30,
+            }
+
+        def colorstr(self, astr, color):
+            return '\033[{}m{}\033[{}m'.format(self.COLOR_CODE[color], astr, self.COLOR_CODE['ENDC'])
+
+        def format(self, record):
+            record = copy.copy(record)
+            levelname = record.levelname
+            if levelname in self.LEVEL_COLOR:
+                record.levelname = self.colorstr(levelname, self.LEVEL_COLOR[levelname])
+                record.name = self.colorstr(record.name, 'BOLD')
+                record.msg = self.colorstr(record.msg, self.LEVEL_COLOR[levelname])
+            return logging.Formatter.format(self, record)
+
+
     def _set_logger(self, logfile=None):
         # create a logger, but shutdown the previous one
         if self._logger is not None:
@@ -328,7 +394,7 @@ class RuntimeEnvironments(object):
         }
         #
         cout.setLevel(levels[self._verbosity])
-        cout.setFormatter(ColoredFormatter('%(levelname)s: %(message)s'))
+        cout.setFormatter(self.ColoredFormatter('%(levelname)s: %(message)s'))
         self._logger.addHandler(cout)
         # output to a log file
         if logfile is not None:
@@ -343,6 +409,9 @@ class RuntimeEnvironments(object):
 
 # the singleton object of RuntimeEnvironments
 env = RuntimeEnvironments()
+# create a default logger without logging to file, this makes sure a logger
+# will be usable even when a project is failed to create
+env.logger = None
 
 SQL_KEYWORDS = set([
     'ADD', 'ALL', 'ALTER', 'ANALYZE', 'AND', 'AS', 'ASC', 'ASENSITIVE', 'BEFORE',
@@ -414,72 +483,6 @@ def encodeTableName(name):
 #
 # Utility functions
 #
-class ColoredFormatter(logging.Formatter):
-    # A variant of code found at http://stackoverflow.com/questions/384076/how-can-i-make-the-python-logging-output-to-be-colored
-    def __init__(self, msg):
-        logging.Formatter.__init__(self, msg)
-        self.LEVEL_COLOR = {
-            'DEBUG': 'BLUE',
-            'WARNING': 'PURPLE',
-            'ERROR': 'RED',
-            'CRITICAL': 'RED_BG',
-            }
-        self.COLOR_CODE={
-            'ENDC':0,  # RESET COLOR
-            'BOLD':1,
-            'UNDERLINE':4,
-            'BLINK':5,
-            'INVERT':7,
-            'CONCEALD':8,
-            'STRIKE':9,
-            'GREY30':90,
-            'GREY40':2,
-            'GREY65':37,
-            'GREY70':97,
-            'GREY20_BG':40,
-            'GREY33_BG':100,
-            'GREY80_BG':47,
-            'GREY93_BG':107,
-            'DARK_RED':31,
-            'RED':91,
-            'RED_BG':41,
-            'LIGHT_RED_BG':101,
-            'DARK_YELLOW':33,
-            'YELLOW':93,
-            'YELLOW_BG':43,
-            'LIGHT_YELLOW_BG':103,
-            'DARK_BLUE':34,
-            'BLUE':94,
-            'BLUE_BG':44,
-            'LIGHT_BLUE_BG':104,
-            'DARK_MAGENTA':35,
-            'PURPLE':95,
-            'MAGENTA_BG':45,
-            'LIGHT_PURPLE_BG':105,
-            'DARK_CYAN':36,
-            'AUQA':96,
-            'CYAN_BG':46,
-            'LIGHT_AUQA_BG':106,
-            'DARK_GREEN':32,
-            'GREEN':92,
-            'GREEN_BG':42,
-            'LIGHT_GREEN_BG':102,
-            'BLACK':30,
-        }
-
-    def colorstr(self, astr, color):
-        return '\033[{}m{}\033[{}m'.format(self.COLOR_CODE[color], astr, self.COLOR_CODE['ENDC'])
-
-    def format(self, record):
-        record = copy.copy(record)
-        levelname = record.levelname
-        if levelname in self.LEVEL_COLOR:
-            record.levelname = self.colorstr(levelname, self.LEVEL_COLOR[levelname])
-            record.name = self.colorstr(record.name, 'BOLD')
-            record.msg = self.colorstr(record.msg, self.LEVEL_COLOR[levelname])
-        return logging.Formatter.format(self, record)
-
-
 def lineCount(filename, encoding='UTF-8'):
     '''Estimate the number of lines using file size and line size. This
     function does not attemp to calculate line count exactly because files
