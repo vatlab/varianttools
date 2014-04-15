44eb03ca129c0e1fa8f27f9f9b1f400a230517fe
bpeng2000 <bpeng2000@bfd2a824-b778-4082-b236-bff916e0c2bb>
2014-04-15 12:30:33 +0000

    Mutational space srv2, not working yet

--- source/srv.py
+++ source/srv.py
@@ -305,6 +305,214 @@ options = [
 ]
 
 
+class   RandomFitness:
+    def __init__(self):
+        self.coefMap = {}
+
+    def _newS(self, loc, alleles):
+        raise ValueError("This function should be redefined in the subclass of this class")
+
+    def __call__(self, loc, alleles):
+        # because s is assigned for each locus, we need to make sure the
+        # same s is used for fitness of genotypes 01 (1-s) and 11 (1-2s)
+        # at each locus
+        if loc in self.coefMap:
+            s, h = self.coefMap[loc]
+        else:
+            res = self._newS(loc, alleles)
+            if type(res) in [list, tuple]:
+                if len(res) != 2:
+                    raise ValueError("A list or tuple of s, h is expected")
+                s, h = res
+            else:
+                s = res
+                h = self.h
+            #
+            self.coefMap[loc] = s, h
+        if 0 in alleles:
+            return 1. - s * h
+        else:
+            return 1. - s        
+
+
+class ConstantFitness(RandomFitness):
+    def __init__(self, s, h=0.5):
+        RandomFitness.__init__(self)
+        self.s = s
+        self.h = h
+
+    def _newS(self, loc, alleles):
+        # each mutant gets a penalty of s
+        return self.s, self.h
+
+class CustomizedFitness(RandomFitness):
+    def __init__(self, func):
+        RandomFitness.__init__(self)
+        self.func = func
+        self.h = 0.5   # default value that can be overwritten
+
+    def _newS(self, loc, alleles):
+        # each mutant gets a penalty of s
+        return self.func(loc, alleles)
+
+class GammaDistributedFitness(RandomFitness):
+    def __init__(self, k, theta, h=0.5):
+        RandomFitness.__init__(self)
+        self.k = k
+        self.theta = theta
+        self.h = h
+
+    def _newS(self):
+        return sim.getRNG().randGamma(self.k, self.theta)
+
+class BoundedMixedGammaDistributedFitness(RandomFitness):
+    def __init__(self, k, theta, h=0.5, p=0, a=0, lower=-1000, upper=10000):
+        RandomFitness.__init__(self)
+        self.k = k
+        self.theta = theta
+        self.h = h
+        self.p = p
+        self.a = a
+        self.lower = lower
+        self.upper = upper
+     
+    def _newS(self):
+        if p > 0 and sim.getRNG().randUniform() < p:
+            return a
+        while True:
+            s = sim.getRNG().randGamma(self.k, self.theta)
+            if s >= self.lower and s <= self.upper:
+                return s
+        
+class TrimmedMixedGammaDistributedFitness(RandomFitness):
+    def __init__(self, k, theta, h=0.5, p=0, a=0, lower=-1000, upper=10000):
+        RandomFitness.__init__(self)
+        self.k = k
+        self.theta = theta
+        self.h = h
+        self.p = p
+        self.a = a
+        self.lower = lower
+        self.upper = upper
+     
+    def _newS(self):
+        if p > 0 and sim.getRNG().randUniform() < p:
+            return a
+        s = sim.getRNG().randGamma(self.k, self.theta)
+        if s >= self.lower and s <= self.upper:
+            return s
+        elif s < self.lower:
+            return 0
+        else:
+            return 1
+
+def getSelector(selDist, selCoef, selModel='exponential'):
+    # set default parameter
+    if selCoef is None:
+        # set default parameters
+        if selDist == 'mixed_gamma':
+            selCoef = (0.0186, 0.0001, 0.184, 0.160*2, 0.5, 0.0001, 0.1)
+        elif selDist == 'mixed_gamma1':
+            selCoef = (0, -1, 0.562341, 0.01, 0.5, 0.00001, 0.1)
+        elif selDist == 'gamma1':
+            selCoef = (0.23, 0.185*2, 0.5)
+        elif selDist == 'gamma2':
+            selCoef = (0.184, 0.160*2, 0.5)
+        elif selDist == 'gamma3':
+            selCoef = (0.206, 0.146*2, 0.5)
+        elif selDist == 'constant':
+            selCoef = [0.01, 0.5]
+        elif not callable(selDist):
+            raise ValueError("Unsupported random distribution")
+    #
+    # use a right selection operator.
+    mode = {'multiplicative': sim.MULTIPLICATIVE,
+        'additive': sim.ADDITIVE,
+        'exponential': sim.EXPONENTIAL}[selModel]
+    #
+    #
+    if callable(selDist):
+        pyFunc = CustomizedFitness(selDist)
+    elif selDist == 'mixed_gamma':
+        pyFunc = BoundedMixedGammaDistributedFitness(k=selCoef[2], theta=selCoef[3],
+            h=selCoef[4], p=selCoef[0], a=selfCoef[1], lower=selCoef[5], upper=selCoef[6])
+    elif selDist == 'mixed_gamma1':
+        pyFunc = TrimmedMixedGammaDistributedFitness(k=selCoef[2], theta=selCoef[3],
+            h=selCoef[4], p=selCoef[0], a=selCoef[1], lower=selCoef[5], upper=selCoef[6])
+    elif selDist.startswith('gamma'):
+        pyFunc = GammaDistributedFitness(k=selCoef[0], theta=selCoef[1])
+    elif selDist == 'constant':
+        pyFunc = ConstantFitness(s=selCoef[0], h=selCoef[1])
+    #
+    mySelector = sim.PyMlSelector(func=pyFunc, mode=mode)
+
+
+
+def simuRareVariants2(pop, demoModel, mu, selector, recRate=0, 
+        statFile='', popFile='', markerFile='', mutantFile='', genotypeFile='',
+        logger=None):
+    #
+    # Evolve
+    pop.addInfoFields(['fitness', 'migrate_to'])
+    startTime = time.clock()
+    #
+    pop.evolve(
+        initOps=sim.InitSex(),
+        preOps=[
+            sim.PyOutput('''Statistics outputted are
+1. Generation number,
+2. population size (a list),
+3. number of segregation sites,
+4. average number of mutants per individual
+5. average allele frequency * 100
+6. average fitness value
+7. minimal fitness value of the parental population
+''', at = 0) + \
+            # revert alleles at fixed loci to wildtype
+            sim.RevertFixedSites(),
+            # 
+            sim.SNPMutator(u=mu, v=mu),
+            # selection on all loci
+            selector,
+            # output statistics in verbose mode
+            sim.IfElse(verbose > 0, ifOps=[
+                sim.Stat(popSize=True, meanOfInfo='fitness', minOfInfo='fitness',
+                    numOfSegSites=sim.ALL_AVAIL, numOfMutants=sim.ALL_AVAIL),
+                sim.PyEval(r'"%5d %s %5d %.6f %.6f %.6f %.6f\n" '
+                    '% (gen, subPopSize, numOfSegSites, float(numOfMutants)/popSize, '
+                    '(numOfMutants * 50. / numOfSegSites / popSize) if numOfSegSites else 0, '
+                    ' meanOfInfo["fitness"], minOfInfo["fitness"])',
+                    output='>>' + statFile),
+                ], at = progGen
+            ),
+        ],
+        matingScheme=sim.RandomMating(ops=
+            sim.Recombinator(rates=recRate, loci=sim.ALL_AVAIL) if recRate != 0 else sim.MendelianGenoTransmitter(),
+            subPopSize=demoModel),
+        finalOps=[
+            # revert fixed sites so that the final population does not have fixed sites
+            sim.RevertFixedSites(),
+            sim.IfElse(verbose > 0, ifOps=[
+                # statistics after evolution
+                sim.Stat(popSize=True, meanOfInfo='fitness', minOfInfo='fitness',
+                    numOfSegSites=sim.ALL_AVAIL, numOfMutants=sim.ALL_AVAIL),
+                sim.PyEval(r'"%5d %s %5d %.6f %.6f %.6f %.6f\n" '
+                    '% (gen, subPopSize, numOfSegSites, float(numOfMutants)/popSize, '
+                    '(numOfMutants * 50. / numOfSegSites / popSize) if numOfSegSites else 0, '
+                    ' meanOfInfo["fitness"], minOfInfo["fitness"])',
+                    output='>>' + statFile),
+                sim.PyEval(r'"Simulated population has %d individuals, %d segregation sites.'
+                           r'There are on average %.1f mutants per individual. Mean allele frequency is %.4f%%.\n"'
+                           r'% (popSize, numOfSegSites, numOfMutants / popSize, (numOfMutants * 50. / numOfSegSites/ popSize) if numOfSegSites else 0)' ),
+            ]),
+        ],
+        gen = demoModel.num_gens
+    )
+    #
+    env.logger.info('Population simulation takes %.2f seconds' % (time.clock() - startTime))
+    return pop
+
+
 class NumSegregationSites(sim.PyOperator):
     '''A Python operator to count the number of segregation sites (number of
     distinct mutants), average number of segreagation sites of individuals,
