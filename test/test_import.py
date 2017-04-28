3f722c095625582aae7662daa96e4947ef416e93
Ma <jma7@BSTMC07N607BFW5.local>
2017-04-28 09:48:06 -0500

    test import 10

--- test/test_import.py
+++ test/test_import.py
@@ -259,14 +259,14 @@ class TestImport(ProcessTestCase):
             genoInfo={(1, 'GT'): ['-1', '-1'], (1, 'GQ'): ['3', '3'], (1, 'GD'): ['1', '1'], 
                 (1, 'PL_1'): ['None', 'None'], (1, 'PL_2'): ['None', 'None'], (1, 'PL3_1'): ['0', '3']})
 
-    # def testInsertDelete(self):
-    #     'Testing the number of insertions and deletions'
-    #     self.assertSucc('vtools import vcf/SAMP3_complex_variants.vcf --build hg19')
-    #     self.assertOutput('''vtools select variant 'ref="-"' --output chr pos ref alt''', 'output/import_vcf_ref.txt')
-    #     self.assertProj(numOfVariants=134)
-    #     self.assertOutput('''vtools select variant 'ref="-"' --count''', '73\n') 
-    #     self.assertOutput('''vtools select variant 'alt="-"' --output chr pos ref alt''', 'output/import_vcf_alt.txt')
-    #     self.assertOutput('''vtools select variant 'alt="-"' --count''', '53\n') 
+    def testInsertDelete(self):
+        'Testing the number of insertions and deletions'
+        self.assertSucc('vtools import vcf/SAMP3_complex_variants.vcf --build hg19')
+        self.assertOutput('''vtools select variant 'ref="-"' --output chr pos ref alt''', 'output/import_vcf_ref.txt')
+        self.assertProj(numOfVariants=134)
+        self.assertOutput('''vtools select variant 'ref="-"' --count''', '73\n') 
+        self.assertOutput('''vtools select variant 'alt="-"' --output chr pos ref alt''', 'output/import_vcf_alt.txt')
+        self.assertOutput('''vtools select variant 'alt="-"' --count''', '53\n') 
 
     def testSampleName_single(self):
         'Testing the import of sample names'
