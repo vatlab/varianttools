2d6385ea2fe9adca140d665b2a112a97124a03b7
Ma <jma7@BSTMC07N607BFW5.local>
2017-05-01 11:15:04 -0500

    test select 7

--- test/test_select.py
+++ test/test_select.py
@@ -122,15 +122,15 @@ class TestSelect(ProcessTestCase):
     #     self.assertOutput("vtools execute 'select count(*) from sample where sample_name like \"NA0%\"'", '9\n')
     #     self.assertSucc('vtools select CEU -s "BMI<18.5" -t Underweight')
 
-    # def testSelectSampleWithWildtypeGenotype(self):
-    #     self.runCmd('vtools import vcf/with_wildtype.vcf --sample_name WT')
-    #     # original 989 variants but some of them have only wildtype genotype
-    #     self.assertOutput('''vtools select variant --samples "sample_name='WT'" -c''', "934\n")
+    def testSelectSampleWithWildtypeGenotype(self):
+        self.runCmd('vtools import vcf/with_wildtype.vcf --sample_name WT')
+        # original 989 variants but some of them have only wildtype genotype
+        self.assertOutput('''vtools select variant --samples "sample_name='WT'" -c''', "934\n")
 
 
-    # def testSelectLargeSample(self):
-    #     self.runCmd('vtools import vcf/500SAMP.vcf')
-    #     self.assertSucc('vtools select variant --samples "sample_name like \'SAM%\'" -c')
+    def testSelectLargeSample(self):
+        self.runCmd('vtools import vcf/500SAMP.vcf')
+        self.assertSucc('vtools select variant --samples "sample_name like \'SAM%\'" -c')
 
 
     # def testFunctionLeast(self):
