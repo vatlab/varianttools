a27f6e45bd0c2cdf25b6da7dd7a91b1fe8ed038b
Ma <jma7@BCBMC02MG1WJF6T.zyxel.com>
2017-05-03 13:32:55 -0500

    test exclude

--- test/test_exclude.py
+++ test/test_exclude.py
@@ -40,35 +40,35 @@ class TestExclude(ProcessTestCase):
         self.runCmd('vtools use ann/testNSFP.ann')
         self.runCmd('vtools select variant \'testNSFP.chr is not null\' -t ns')
         
-    # def testExclude(self):
-    #     'Test command vtools exclude'
-    #     self.assertFail('vtools exclude')
-        # self.assertSucc('vtools exclude -h')
-        # # Variant table non_existing_table does not exist.
-        # self.assertFail('vtools exclude non_existing_table "aff=\'1\'" -t aff')
-        # # Cannot overwrite master variant table. Please choose another name for the variant table.
-        # self.assertFail('vtools exclude ns "sift_score<=0.94" -t variant')
-        # self.assertSucc('vtools exclude ns "sift_score <= 0.94" -t ns_non_damaging')
-        # should have 5 variants
-        # select "sift_score >= 0.94" will result in 6 variants
-        # self.assertOutput('vtools select ns "sift_score > 0.94" -c', 'output/exclude_sift.txt')
-        # self.assertOutput('vtools exclude ns "sift_score <= 0.94" -c', 'output/exclude_sift.txt')
-        # self.assertOutput('vtools exclude ns "variant_id=604" -c', '1445')
+    def testExclude(self):
+        'Test command vtools exclude'
+        self.assertFail('vtools exclude')
+        self.assertSucc('vtools exclude -h')
+        # Variant table non_existing_table does not exist.
+        self.assertFail('vtools exclude non_existing_table "aff=\'1\'" -t aff')
+        # Cannot overwrite master variant table. Please choose another name for the variant table.
+        self.assertFail('vtools exclude ns "sift_score<=0.94" -t variant')
+        self.assertSucc('vtools exclude ns "sift_score <= 0.94" -t ns_non_damaging')
+        should have 5 variants
+        select "sift_score >= 0.94" will result in 6 variants
+        self.assertOutput('vtools select ns "sift_score > 0.94" -c', 'output/exclude_sift.txt')
+        self.assertOutput('vtools exclude ns "sift_score <= 0.94" -c', 'output/exclude_sift.txt')
+        self.assertOutput('vtools exclude ns "variant_id=604" -c', '1445')
 
 
-    #def testExcludeAnno(self):
-    #    self.runCmd('vtools liftover hg19')
-    #    # FIXME: this test is bad because it requies downloading a large database
-    #    self.runCmd('vtools use dbSNP')
-    #    # this will exclude ones called unknown, but will keep the ones that is NULL
-    #    # and remove variants with more than one entries
-    #    #
-    #    #
-    #    self.assertSucc(''' vtools exclude variant "dbSNP.valid='unknown'" -t known ''')
-    #    self.assertOutput('vtools output known variant_id chr pos ref alt ', 'output/exclude_anno.txt')
-    #    self.assertSucc('vtools update variant --set dbSNP_valid=dbSNP.valid ')
-    #    self.assertOutput('''vtools select variant 'dbSNP.valid <> "unknown" OR dbSNP.valid is NULL' --output variant_id chr pos ref alt''',
-    #        'output/exclude_select_anno.txt')
+    def testExcludeAnno(self):
+       self.runCmd('vtools liftover hg19')
+       # FIXME: this test is bad because it requies downloading a large database
+       self.runCmd('vtools use dbSNP')
+       # this will exclude ones called unknown, but will keep the ones that is NULL
+       # and remove variants with more than one entries
+       #
+       #
+       self.assertSucc(''' vtools exclude variant "dbSNP.valid='unknown'" -t known ''')
+       self.assertOutput('vtools output known variant_id chr pos ref alt ', 'output/exclude_anno.txt')
+       self.assertSucc('vtools update variant --set dbSNP_valid=dbSNP.valid ')
+       self.assertOutput('''vtools select variant 'dbSNP.valid <> "unknown" OR dbSNP.valid is NULL' --output variant_id chr pos ref alt''',
+           'output/exclude_select_anno.txt')
 
 
 if __name__ == '__main__':
