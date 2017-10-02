4f88ac9b2343457d87ed923fcae3fb763eca14e4
Ma <jma7@BSTMC07TP062G1J.local>
2017-10-02 10:41:47 -0500

    change from multipleprocess to multiplethreads

--- src/variant_tools/importer_allele_hdf5.py
+++ src/variant_tools/importer_allele_hdf5.py
@@ -24,6 +24,7 @@ from .liftOver import LiftOverTool
 from .utils import ProgressBar, lineCount, getMaxUcscBin, delayedAction, \
     openFile, DatabaseEngine, hasCommand, \
     downloadFile, env, RefGenome
+import threading
 
 import numpy as np
 import tables as tb
@@ -965,7 +966,8 @@ def _read_vcf_headers(stream):
 
 
 
-class HDF5GenotypeImportWorker(Process):
+# class HDF5GenotypeImportWorker(Process):
+class HDF5GenotypeImportWorker(threading.Thread):
     '''This class starts a process, import genotype to a temporary HDF5 file.
         Args
 
@@ -984,7 +986,8 @@ class HDF5GenotypeImportWorker(Process):
     def __init__(self, chunk,variantIndex, start_sample,end_sample,sample_ids,variant_count, 
         proc_index,geno_info,dbLocation,build):
 
-        Process.__init__(self, name='GenotypeImporter')
+        # Process.__init__(self, name='GenotypeImporter')
+        threading.Thread.__init__(self)
         # self.daemon=True
         self.chunk=chunk
         self.variantIndex = variantIndex
