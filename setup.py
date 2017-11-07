65af497be974aa08e46d0e692ef453e14dafb7dc
Ma <jma7@BSTMC07TP062G1J.local>
2017-11-07 11:38:47 -0600

    check setup

--- setup.py
+++ setup.py
@@ -613,12 +613,11 @@ else:
     libs = []
     gccargs = ['-O3', '-Wno-unused-local-typedef', '-Wno-return-type']
 
-ENV_INCLUDE_DIRS = os.environ.get('LD_INCLUDE_PATH', '').split(os.pathsep)
-ENV_LIBRARY_DIRS = os.environ.get('LD_LIBRARY_PATH', '').split(os.pathsep)
-#ENV_INCLUDE_DIRS = ['']
-#ENV_LIBRARY_DIRS = ['']
-print("ENV_INCLUDE_DIRS",ENV_INCLUDE_DIRS)
-print("ENV_LIBRARY_DIRS",ENV_LIBRARY_DIRS)
+# ENV_INCLUDE_DIRS = os.environ.get('LD_INCLUDE_PATH', '').split(os.pathsep)
+# ENV_LIBRARY_DIRS = os.environ.get('LD_LIBRARY_PATH', '').split(os.pathsep)
+ENV_INCLUDE_DIRS = ['']
+ENV_LIBRARY_DIRS = ['']
+
 
 if EMBEDDED_BOOST:
     try:
