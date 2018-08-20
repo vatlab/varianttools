cff1e8fd8a971134c2e86ef37f717674a947063a
Ma <jma7@mdanderson.org>
2018-08-20 10:37:23 -0500

    change implementation of cluster_runAssociation [ci skip]

--- src/variant_tools/association.py
+++ src/variant_tools/association.py
@@ -1169,48 +1169,48 @@ def cluster_runAssociation(args,asso,proj,results):
         asso.proj.annoDB=""
         asso.tests=""
         outputs=[]
-        # for grp in asso.groups:
-        #     grps.append(grp)
-        #     if count%10==0:
-        #         output=run_grp_association.delay(asso, grps,
-        #             args,os.getcwd())
-        #         # results.record(result.get()[0])
-        #         outputs.append(output)
+        for grp in asso.groups:
+            grps.append(grp)
+            if count%10==0:
+                output=run_grp_association.delay(asso, grps,
+                    args,os.getcwd())
+                # results.record(result.get()[0])
+                outputs.append(output)
                 
-        #         grps=[]
-        #     count+=1
-        #     prog.update(count)
-        # if len(grps)!=0:
-        #     output=run_grp_association.delay(asso, grps,
-        #             args,os.getcwd())
-        #     # results.record(result.get()[0])
-        #     outputs.append(output)
-
-        # while outputs:
-        #     output=outputs.pop(0)
-        #     if output.ready():
-        #         for rec in output.get():
-        #             results.record(rec)
-        #     else:
-        #         outputs.append(output)
-
-        for grp in asso.groups:    
-            output=run_grp_association.delay(asso, [grp],
-                args,os.getcwd())
-            # results.record(result.get()[0])
-            outputs.append(output.task_id)
+                grps=[]
             count+=1
             prog.update(count)
+        if len(grps)!=0:
+            output=run_grp_association.delay(asso, grps,
+                    args,os.getcwd())
+            # results.record(result.get()[0])
+            outputs.append(output)
 
         while outputs:
             output=outputs.pop(0)
-            res=AsyncResult(output)
-            print(output,res.state)
-            if res.ready():
-                rec=res.get()[0]
-                results.record(rec)
+            if output.ready():
+                for rec in output.get():
+                    results.record(rec)
             else:
-               outputs.append(output)
+                outputs.append(output)
+
+        # for grp in asso.groups:    
+        #     output=run_grp_association.delay(asso, [grp],
+        #         args,os.getcwd())
+        #     # results.record(result.get()[0])
+        #     outputs.append(output.task_id)
+        #     count+=1
+        #     prog.update(count)
+
+        # while outputs:
+        #     output=outputs.pop(0)
+        #     res=AsyncResult(output)
+        #     print(output,res.state)
+        #     if res.ready():
+        #         rec=res.get()[0]
+        #         results.record(rec)
+        #     else:
+        #        outputs.append(output)
           
      
         
